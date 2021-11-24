6db93e6b5dad270a9cbc14bef31147b8ef529d65
andresvbse <andres.virguez@bse.eu>
2021-11-24 08:53:58 +0100

    Problem with the code to indicate the class value from prob. solved

--- src/other_functions/customplots.py
+++ src/other_functions/customplots.py
@@ -3,7 +3,12 @@ import pandas as pd
 
 import matplotlib.pylab as plt
 import seaborn as sns
-from sklearn.metrics import roc_curve, auc
+
+from sklearn.metrics import roc_curve, auc, roc_auc_score
+from sklearn.preprocessing import label_binarize
+
+
+from itertools import cycle
 
 def missings_plot(df):
     missinvalues = df.isnull().sum(axis=0)/df.shape[0]
@@ -77,3 +82,72 @@ def grid_serach_dt(scores, std_error, alphas, best_params):
     ymin, ymax = plt.ylim()
     plt.vlines(best_params ,ymin, ymax, linestyle='dashed')
     plt.xlim([alphas[0], alphas[-1]])
+
+# Compute ROC curve and ROC area for each class
+def roc_auc_multiclass_plot(y, y_hat, n_classes):
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = roc_curve(y[:, i], y_hat[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+
+    # Compute micro-average ROC curve and ROC area
+    fpr["micro"], tpr["micro"], _ = roc_curve(y.ravel(), y_hat.ravel())
+    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+    
+    lw = 2
+
+    all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+
+    # Then interpolate all ROC curves at this points
+    mean_tpr = np.zeros_like(all_fpr)
+    for i in range(n_classes):
+        mean_tpr += np.interp(all_fpr, fpr[i], tpr[i])
+
+    # Finally average it and compute AUC
+    mean_tpr /= n_classes
+
+    fpr["macro"] = all_fpr
+    tpr["macro"] = mean_tpr
+    roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+
+    # Plot all ROC curves
+    plt.figure(figsize=(9,6))
+
+    plt.plot(
+        fpr["micro"],
+        tpr["micro"],
+        label="micro-average ROC curve (area = {0:0.2f})".format(roc_auc["micro"]),
+        color="deeppink",
+        linestyle=":",
+        linewidth=4,
+    )
+
+    plt.plot(
+        fpr["macro"],
+        tpr["macro"],
+        label="macro-average ROC curve (area = {0:0.2f})".format(roc_auc["macro"]),
+        color="navy",
+        linestyle=":",
+        linewidth=4,
+    )
+
+    colors = cycle(["aqua", "darkorange", "cornflowerblue", "seagreen", "indigo", "darkgoldenrod", "dimgray"])
+    for i, color in zip(range(n_classes), colors):
+        plt.plot(
+            fpr[i],
+            tpr[i],
+            color=color,
+            lw=lw,
+            label="ROC curve of class {0} (area = {1:0.2f})".format(i+1, roc_auc[i]),
+        )
+
+    plt.plot([0, 1], [0, 1], "k--", lw=lw)
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.title("Some extension of Receiver operating characteristic to multiclass")
+    plt.legend(loc="lower right")
+    plt.show()
